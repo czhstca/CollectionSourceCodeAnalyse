357d9263a5550e7f9875cbe519e2921b04427769
EX_WLJR_CHENZEHUA <EX_WLJR_CHENZEHUA@HQ-PAB40864.sdb.local>
2017-11-30 17:45:43 +0800

    完成TreeMap源码分析

--- CollectionSourceCodeAnalyse/src/sourcecodeanalysis/TreeMapAnalysis.java
+++ CollectionSourceCodeAnalyse/src/sourcecodeanalysis/TreeMapAnalysis.java
@@ -4,6 +4,7 @@ import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
+import java.util.TreeMap;
 import java.util.TreeMap.Entry;
 
 /**
@@ -50,15 +51,6 @@ implements NavigableMap<K,V>, Cloneable, java.io.Serializable {
 	 * 9.TreeMap的优点:对于一些统计操作性能较好，时间复杂度为O(log n).
 	 * 
 	 * 10.TreeMap的缺点:对于增删改查的操作性能较差，时间复杂度同样为O(log n).
-	 * 
-	 * 11.
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
 	 * 
 	 */
 	
@@ -190,7 +182,74 @@ implements NavigableMap<K,V>, Cloneable, java.io.Serializable {
     
     
     /**
-     * 插入节点的操作
+     * 获取节点的操作
+     * 如果在map中找到了这个键，则返回键对应的值；找不到对应的键或者键指向为null，否则返回null
+     *
+     * @throws ClassCastException 用户传入的key和当前map中的key无法比较（不是同一类型），则报该异常
+     * @throws NullPointerException 使用了自然排序但传入的key为null，或者比较器不支持key为null的情况，则报此异常
+     *         
+     */
+    public V get(Object key) {
+        Entry<K,V> p = getEntry(key);   //可以看到实际上是调用getEntry()这个方法来获取节点的
+        return (p==null ? null : p.value);
+    }
+    
+    
+    /**
+     * 返回通过传入的键在map中找到的相应entry，若未找到则返回null
+     *
+     * @throws ClassCastException 用户传入的key和当前map中的key无法比较（不是同一类型），则报该异常
+     * @throws NullPointerException 使用了自然排序但传入的key为null，或者比较器不支持key为null的情况，则报此异常
+     */
+    final Entry<K,V> getEntry(Object key) {
+        // 首先要区分是使用map键的自然排序查找还是使用用户自定义的比较器来进行查找
+        if (comparator != null)  //如果用户传入了自定义比较器
+            return getEntryUsingComparator(key); //调用getEntryUsingComparator()方法，通过用户自定义比较器的compare()方法查找entry
+        if (key == null)  //如果key为null，报空指针异常
+            throw new NullPointerException();
+        @SuppressWarnings("unchecked")
+            Comparable<? super K> k = (Comparable<? super K>) key;  //使用key的自然排序比较传入的key和map中的key
+        Entry<K,V> p = root;  //从根节点开始比较
+        while (p != null) {  //如果节点不是空，则一直循环遍历比较
+            int cmp = k.compareTo(p.key); //获取传入的key和当前节点key的比较结果,使用自然排序Comparable实现的compareTo()方法进行比较
+            if (cmp < 0)  //结果<0,说明传入的key比当前节点的key小
+                p = p.left; //将下次比较的节点变更为当前节点的左子节点
+            else if (cmp > 0)  //结果>0,说明传入的key比当前节点的key大
+                p = p.right; //将下次比较的节点变更为当前节点的右子节点
+            else  //结果相等,则该节点就是要查找的节点
+                return p;  //直接返回节点对应的Entry
+        }
+        return null;   //没找到则返回null
+    }
+    
+    
+
+    /**
+     * 使用用户自定义比较器的比较方法来查找节点
+     * 逻辑与通过自然排序查找类似，不再赘述
+     */
+    final Entry<K,V> getEntryUsingComparator(Object key) {
+        @SuppressWarnings("unchecked")
+            K k = (K) key;
+        Comparator<? super K> cpr = comparator;
+        if (cpr != null) {
+            Entry<K,V> p = root;
+            while (p != null) {
+                int cmp = cpr.compare(k, p.key);  //仅仅这里和getEntry()方法不同，使用自定义比较器的compare()方法进行比较
+                if (cmp < 0)
+                    p = p.left;
+                else if (cmp > 0)
+                    p = p.right;
+                else
+                    return p;
+            }
+        }
+        return null;
+    }
+    
+    
+    /**
+     * 插入节点,并平衡红黑树的操作
      * 如果原先map中已经有该key对应的键值对，则替换原先该key对应的value为新的value
      * 如果原先map中没有该key对应的键值对，则在map中新插入一个该key和value对应的键值对
      *
@@ -258,6 +317,109 @@ implements NavigableMap<K,V>, Cloneable, java.io.Serializable {
         modCount++;  //结构性操作计数器 +1
         return null;  //返回null
     }
+    
+    
+    
+    
+    /**
+     * 删除节点的相关操作
+     * 删除该key在map中对应的Entry(如果有的话)
+     *
+     * @param  要删除键值对的key
+     * @return 被删除键值对原先的值
+     * @throws ClassCastException 如果用户传入了一个不可和其他key比较的key（违反泛型约定），则抛出该异常
+     * @throws NullPointerException key传入了null，则报此异常
+     */
+    public V remove(Object key) {
+        Entry<K,V> p = getEntry(key);  //先调用getEntry()方法找到该key在map中对应的Entry
+        if (p == null)   //如果没找到
+            return null;  //返回null
+
+        V oldValue = p.value;   //找到了先把键值对的值保存下来供后续返回
+        deleteEntry(p);  //调用deleteEntry()方法，删除键值对
+        return oldValue;  //返回该键值对原先的值
+    }
+    
+    
+    /**
+     * 删除节点，并平衡红黑树的操作(可参考deleteEntry方法流程图).
+     * 
+     * @Param Entry<K,V> p  要删除的节点Entry
+     */
+    private void deleteEntry(Entry<K,V> p) {
+        modCount++;   //结构性操作计数器加1
+        size--;  //节点总数-1
+
+        if (p.left != null && p.right != null) {  //当前要删除的节点左右子节点都不为空
+            Entry<K,V> s = successor(p);  //找到一个待删除节点的继承者节点s
+            //将指向s节点,后续所有对p的节点判断其实都是对s节点判断
+            p.key = s.key;
+            p.value = s.value;
+            p = s;
+        }
+
+        //替代节点选择为当前被删除节点的左子节点(优先)或右子节点
+        Entry<K,V> replacement = (p.left != null ? p.left : p.right);
+
+        if (replacement != null) {  //替代节点(被删除节点的子节点)不为空
+            
+            replacement.parent = p.parent;  //将替代节点的父节点指向被删除节点的父节点
+            if (p.parent == null)  //如果被删除节点的父节点为null (即被删除的节点就是树的根节点,且根节点下面还有其他节点)
+                root = replacement;  //将根节点设置为替换节点
+            else if (p == p.parent.left)  //如果原先被删除节点是左子树 插入
+                p.parent.left  = replacement;  //则将替换节点也保持左子树插入（将替换节点与被删除节点的父节点左子节点建立引用）
+            else   //如果原先被删除节点是右子树 插入
+                p.parent.right = replacement;  //则将替换节点也保持右子树插入（将替换节点与被删除节点的父节点右子节点建立引用）
+
+            //将被删除节点的左子节点、右子节点、父节点引用全部置为null
+            p.left = p.right = p.parent = null;
+
+            //删除后要执行后续的保证红黑树约定的操作
+            if (p.color == BLACK)  //如果被删除节点是黑色的
+                fixAfterDeletion(replacement);  //调用删除后修正红黑树规则的方法
+        } else if (p.parent == null) { //被删除节点就是根节点，且整个树中就只有一个根节点的情况
+            root = null;  //将根节点置为null（此时整个树中就没有节点了）
+        } else {  //被删除节点没有子节点可替代的情况 (被删除节点是叶子节点)
+            if (p.color == BLACK)   //如果被删除节点是黑色的
+                fixAfterDeletion(p);  //调用删除后修正红黑树规则的方法
+
+            if (p.parent != null) {  //如果被删除节点的父节点不为null
+                if (p == p.parent.left) //如果原先被删除节点是左子树 插入
+                    p.parent.left = null;  //删除节点后将被删除节点的父节点的左子节点置为null
+                else if (p == p.parent.right)  //如果原先被删除节点是右子树 插入
+                    p.parent.right = null;  //删除节点后将被删除节点的父节点的右子节点置为null
+                p.parent = null;  //将被删除节点的父节点引用置为null(即将被删除节点从树中移除)
+            }
+        }
+    }
+    
+    
+    /**
+     * 返回被删除节点的继承者节点(可参考选择继承者方法流程图)
+     */
+    static <K,V> TreeMap.Entry<K,V> successor(Entry<K,V> t) {
+        if (t == null)  //如果被删除节点为空，则直接返回null
+            return null;
+        else if (t.right != null) {  //如果被删除节点的右子节点不为空
+            Entry<K,V> p = t.right;  //将被删除节点的右子节点记录下来
+            while (p.left != null)  //从该节点开始循环向下查找左子节点，直至找到叶子节点后返回该叶子节点
+                p = p.left;
+            return p;
+        } else {  //如果被删除节点的右子节点为空
+            Entry<K,V> p = t.parent;  //将被删除节点的父节点用p变量记录
+            Entry<K,V> ch = t;   //被删除节点用ch变量记录
+            while (p != null && ch == p.right) {//从被删除节点开始循环向上查找父节点，直到父节点为空或者父节点没有右子节点，返回该父节点
+                ch = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
+    
+    
+    
+    
+    
     
     /**  平衡树的相关操作  */
     
@@ -335,7 +497,7 @@ implements NavigableMap<K,V>, Cloneable, java.io.Serializable {
     	//因为红黑树有一个特性: "从根节点到所有叶子节点上的黑色节点数量是相同的"
     	//如果当前插入的节点是黑色的，那么必然会违反这个特性，所以必须将插入节点的颜色先设置为红色
     	x.color = RED;
-    	//第一次遍历时，x变量保存的为当前新插入的节点
+    	//第一次遍历时，x变量保存的是当前新插入的节点
     	//为什么要用while循环?
     	//因为在旋转的过程中可能还会出现父子节点均为红色的情况，所以要不断往上遍历直至整个树都符合红黑树的规则
         while (x != null && x != root && x.parent.color == RED) { //如果当前节点不为空且不是根节点，并且当前节点的父节点颜色为红色
@@ -380,7 +542,72 @@ implements NavigableMap<K,V>, Cloneable, java.io.Serializable {
     }
     
     
-    
+    /** 
+     * 树删除一个节点后，将其根据红黑树的规则进行修正(可参考fixAfterDeletion方法流程图)
+     * @param x  当前删除的节点
+     */
+    private void fixAfterDeletion(Entry<K,V> x) {
+       //循环遍历，x刚开始为被删除的节点
+    	while (x != root && colorOf(x) == BLACK) {  //如果当前遍历到的节点不是根节点且为黑色
+            if (x == leftOf(parentOf(x))) {  //如果当前遍历到的节点是其父节点的左子节点
+                Entry<K,V> sib = rightOf(parentOf(x));  //将当前遍历到的节点的父节点的右子节点用sib变量保存(即和当前节点平级的另一个节点)
+
+                if (colorOf(sib) == RED) {  //如果sib引用的节点是红色的
+                    setColor(sib, BLACK);  //将sib引用的节点设置为黑色
+                    setColor(parentOf(x), RED);  //将当前遍历到节点的父节点设置为红色
+                    rotateLeft(parentOf(x));  //对当前遍历到节点的父节点进行一次左旋操作
+                    sib = rightOf(parentOf(x)); //sib引用的节点变更为旋转后被删除节点的父节点的右子节点
+                }
+
+                if (colorOf(leftOf(sib))  == BLACK &&
+                    colorOf(rightOf(sib)) == BLACK) { //如果sib引用节点的左、右子节点都是黑色的
+                    setColor(sib, RED);  //将sib引用的节点设置为红色
+                    x = parentOf(x);  //下一次遍历的节点变更为当前遍历到节点的父节点
+                } else {  //如果sib引用节点的左、右子节点不全是黑色的
+                    if (colorOf(rightOf(sib)) == BLACK) {  //如果sib引用节点的右子节点是黑色的
+                        setColor(leftOf(sib), BLACK);  //将sib引用节点的左子节点设置为黑色
+                        setColor(sib, RED);   //sib引用节点设置为红色
+                        rotateRight(sib);  //对sib节点进行一次右旋操作
+                        sib = rightOf(parentOf(x)); //sib引用的节点变更为当前遍历到的节点的父节点的右子节点
+                    }
+                    setColor(sib, colorOf(parentOf(x)));  //将sib引用节点的颜色设置为 当前遍历到节点的父节点 一样的颜色
+                    setColor(parentOf(x), BLACK);  //将当前遍历到节点的父节点设置为黑色
+                    setColor(rightOf(sib), BLACK);  //将sib引用节点的右子节点设置为黑色
+                    rotateLeft(parentOf(x));  //对当前遍历到的节点的父节点进行一次左旋操作
+                    x = root;  //下一次遍历的节点变更为根节点
+                }
+            } else { // 当前遍历到的节点是其父节点的右子节点，和上述情况相似，不再赘述
+                Entry<K,V> sib = leftOf(parentOf(x));
+
+                if (colorOf(sib) == RED) {
+                    setColor(sib, BLACK);
+                    setColor(parentOf(x), RED);
+                    rotateRight(parentOf(x));
+                    sib = leftOf(parentOf(x));
+                }
+
+                if (colorOf(rightOf(sib)) == BLACK &&
+                    colorOf(leftOf(sib)) == BLACK) {
+                    setColor(sib, RED);
+                    x = parentOf(x);
+                } else {
+                    if (colorOf(leftOf(sib)) == BLACK) {
+                        setColor(rightOf(sib), BLACK);
+                        setColor(sib, RED);
+                        rotateLeft(sib);
+                        sib = leftOf(parentOf(x));
+                    }
+                    setColor(sib, colorOf(parentOf(x)));
+                    setColor(parentOf(x), BLACK);
+                    setColor(leftOf(sib), BLACK);
+                    rotateRight(parentOf(x));
+                    x = root;
+                }
+            }
+        }
+
+        setColor(x, BLACK);
+    }
     
     
     
